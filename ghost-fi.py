d3fe003c2733c5e6e1f8642b94ed0e9689e0df4d
4nonymus.Coder <149686654+MohammedAbdulAhadSaud@users.noreply.github.com>
2025-09-18 20:51:47 +0530

    Minor Fixes

--- ghost-fi.py
+++ ghost-fi.py
@@ -27,7 +27,7 @@ def network_exist(ssid, bssid):
 
 		output = result.stdout.strip()
 		if not output:
-			print( Fore.YELLOW+"=> No Wi-Fi networks found by nmcli.")
+			print( Fore.YELLOW+"=> No Wi-Fi networks found by nmcli."+ Style.RESET_ALL)
 			return False
 
 		print("DEBUG: Raw nmcli output:")
@@ -51,17 +51,17 @@ def network_exist(ssid, bssid):
 				print("DEBUG: Network found!")
 				return True
 
-		print(Fore.BLUE+"DEBUG: Network not found.")
+		print(Fore.BLUE+"DEBUG: Network not found."+ Style.RESET_ALL)
 		return False
 
 	except subprocess.CalledProcessError as e:
-		print(Fore.RED + f"=> Error fetching Wi-Fi list: {e.stderr.strip()}")
+		print(Fore.RED + f"=> Error fetching Wi-Fi list: {e.stderr.strip()}"+ Style.RESET_ALL)
 		return False
 	except FileNotFoundError:
-		print(  Fore.YELLOW+"=> Error: nmcli command not found. Please ensure NetworkManager is installed.")
+		print(  Fore.YELLOW+"=> Error: nmcli command not found. Please ensure NetworkManager is installed."+ Style.RESET_ALL)
 		return False
 	except Exception as e:
-		print( Fore.RED + f"=>  Unexpected error: {e}")
+		print( Fore.RED + f"=>  Unexpected error: {e}"+ Style.RESET_ALL)
 		return False
 
 #-----------------------------------------------------------------------------------------------------------------------------#
@@ -83,12 +83,12 @@ def list_available_networks():
 		print(result.stdout)
 		print("----------------------------------------------\n")
 	except subprocess.CalledProcessError as e:
-		print( Fore.RED+f"=> Error listing Wi-Fi networks: {e.stderr}")
-		print( Fore.LIGHTYELLOW_EX+"	Please ensure NetworkManager is running and you have appropriate permissions.")
+		print( Fore.RED+f"=> Error listing Wi-Fi networks: {e.stderr}"+ Style.RESET_ALL)
+		print( Fore.LIGHTYELLOW_EX+"	Please ensure NetworkManager is running and you have appropriate permissions."+ Style.RESET_ALL)
 	except FileNotFoundError:
-		print( Fore.LIGHTYELLOW_EX+ "=>  Error: 'nmcli' command not found. Please ensure NetworkManager is installed.")
+		print( Fore.LIGHTYELLOW_EX+ "=>  Error: 'nmcli' command not found. Please ensure NetworkManager is installed."+ Style.RESET_ALL)
 	except Exception as e:
-		print( f"=>  An unexpected error occurred while listing networks: {e}")
+		print( f"=>  An unexpected error occurred while listing networks: {e}"+ Style.RESET_ALL)
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
@@ -102,14 +102,14 @@ def connect_with_profile(ssid, bssid,path="/usr/share/wordlists/rockyou.txt"):
 			content = file.read().decode('utf-8', errors='ignore')
 			passwords = [line.strip() for line in content.splitlines() if line.strip()]
 	except FileNotFoundError:
-		print( Fore.YELLOW+f"=>  Error: Password file not found at '{path}'. Please ensure the path is correct.")
+		print( Fore.YELLOW+f"=>  Error: Password file not found at '{path}'. Please ensure the path is correct."+ Style.RESET_ALL)
 		return
 	except Exception as e:
-		print( Fore.RED + f"=>  Error reading password file: {e}")
+		print( Fore.RED + f"=>  Error reading password file: {e}"+ Style.RESET_ALL)
 		return
 
 	if not passwords:
-		print(Fore.YELLOW+f" > Warning: No passwords found in the file '{path}'.")
+		print(Fore.YELLOW+f" > Warning: No passwords found in the file '{path}'."+ Style.RESET_ALL)
 		return
 	print("\n-------------------------------------------------------------------------------------------------------")
 	print( f"Attempting to connect to Wi-Fi network: '{ssid}'")
@@ -179,7 +179,7 @@ def connect_with_profile(ssid, bssid,path="/usr/share/wordlists/rockyou.txt"):
 			continue
 
 		except subprocess.TimeoutExpired:
-			print( Fore.LIGHTYELLOW_EX+ f"=>  Connection attempt timed out after 20 seconds.")
+			print( Fore.LIGHTYELLOW_EX+ f"=>  Connection attempt timed out after 20 seconds."+ Style.RESET_ALL)
 			continue
 
 		except Exception as e:
@@ -201,7 +201,7 @@ if __name__ == "__main__":
 	figlet = Figlet(font='slant')
 	ascii_art = figlet.renderText('\n      GHOST-FI       ')
 	print(Fore.LIGHTRED_EX + ascii_art + Style.RESET_ALL)
-	print(Fore.LIGHTCYAN_EX +"\n"*5+ " " *40+  "Created by: MohammedAbdulAhadSaud\n")
+	print(Fore.LIGHTCYAN_EX +"\n"*5+ " " *40+  "Created by: MohammedAbdulAhadSaud\n"+ Style.RESET_ALL)
 	print(Fore.LIGHTYELLOW_EX + " " * 40 + "GitHub: https://github.com/MohammedAbdulAhadSaud/Ghost-Fi\n" + Style.RESET_ALL)
 	
 
@@ -209,7 +209,7 @@ if __name__ == "__main__":
 	list_available_networks()
 	ssid = input(Fore.LIGHTWHITE_EX +" > Enter SSID (required):  ").strip()
 	bssid= input(Fore.LIGHTWHITE_EX +" > Enter BSSID (requried):  ").strip()
-	path_input = input(Fore.LIGHTWHITE_EX +" > Enter path to password file (Press Enter to use default rockyou.txt): ")
+	path_input = input(Fore.LIGHTWHITE_EX +" > Enter path to password file (Press Enter to use default rockyou.txt): "+ Style.RESET_ALL)
 	print(Style.RESET_ALL+"\n-------------------------------------------------------------------------------------------------------")
 	path= path_input if path_input else "/usr/share/wordlists/rockyou.txt"
 
@@ -218,7 +218,7 @@ if __name__ == "__main__":
 		print( Fore.RED+"=>  SSID and BSSID are required to proceed.")
 
 	elif not network_exist(ssid, bssid):
-		print( Fore.RED+f"=>  The network '{ssid}' with BSSID '{bssid}' is not currently visible.")
+		print( Fore.RED+f"=>  The network '{ssid}' with BSSID '{bssid}' is not currently visible."+Style.RESET_ALL)
 
 	else:
 		print( Fore.BLUE+f"=>  Network '{ssid}' with BSSID '{bssid}' found — proceeding to connect..."+Style.RESET_ALL)
